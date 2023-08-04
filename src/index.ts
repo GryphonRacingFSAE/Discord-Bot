44a4938bc34e9f1f9e754fbc6fe5bb9beca8a96f
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-08-03 22:15:18 -0400

    fix: compatability and improve the CI a little

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,14 @@ const client = new DiscordClient({ intents: [GatewayIntentBits.Guilds] });
 // Load in commands
 {
     // Iterate over every command/folder
-    const command_sources = ["./commands/countdown", "./commands/ping", "./commands/save-audit-logs"] as const;
+    const command_directory = path.join(__dirname, "commands");
+    const command_sources = fs.readdirSync(command_directory).filter(file => file.endsWith(".js"));
 
     // Iterate over file in said folder
     for (const file of command_sources) {
-        const command: Command = (await import(file)).default;
+        const command_path = path.join(command_directory, file);
+        const resolved_path = pathToFileURL(command_path).href;
+        const command: Command = (await import(resolved_path)).default;
         // Set a new item in the Collection with the key as the command name and the value as the exported module
         client.commands.set(command.data.name, command);
         console.log(`Loaded command ${command.data.name}`);
@@ -28,14 +31,16 @@ const client = new DiscordClient({ intents: [GatewayIntentBits.Guilds] });
 
 // Load in events
 {
-    const eventsPath = path.join(__dirname, "events");
-    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".mjs"));
-    // Iterate over each events/*.mts file
-    for (const file of eventFiles) {
+    const event_directory = path.join(__dirname, "events");
+    const event_files = fs.readdirSync(event_directory).filter(file => file.endsWith(".js"));
+
+    // Iterate over each events file
+    for (const file of event_files) {
         // Import files' content
-        const filePath = path.join(eventsPath, file);
-        const resolvedPath = pathToFileURL(filePath).href;
-        const event: Event = (await import(resolvedPath)).default;
+        const event_path = path.join(event_directory, file);
+        const resolved_path = pathToFileURL(event_path).href;
+        const event: Event = (await import(resolved_path)).default;
+
         // Attach the event onto the client
         if (event.once) {
             client.once(event.name, (...args) => event.execute(...args));
