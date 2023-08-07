f3e7fbeb4134700d1524b149d21475ec05e22700
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-08-07 15:48:42 -0400

    chore: remove leftovers

--- src/events/member-update.ts
+++ src/events/member-update.ts
@@ -48,17 +48,5 @@ export default {
     once: false,
     async execute(old_member: GuildMember, new_member: GuildMember) {
         await updateSubsectionRoles(new_member);
-
-        // If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
-        const removedRoles = old_member.roles.cache.filter(role => !new_member.roles.cache.has(role.id));
-        if (removedRoles.size > 0) {
-            console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${old_member.displayName}.`);
-        }
-
-        // If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
-        const addedRoles = new_member.roles.cache.filter(role => !old_member.roles.cache.has(role.id));
-        if (addedRoles.size > 0) {
-            console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${old_member.displayName}.`);
-        }
     },
 };
