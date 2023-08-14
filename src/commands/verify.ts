c60dfb809e60b7e678a5e502e5236480816dc2b4
Danny Le <git@dannydoes.graphics>
2023-08-14 15:54:55 -0400

    feat: Add more descriptive error messages

--- src/commands/verify.ts
+++ src/commands/verify.ts
@@ -4,7 +4,7 @@ export default {
     data: new SlashCommandBuilder().setName("verify").setDescription("You should verify yourself NOW!"),
     async execute(interaction: CommandInteraction) {
         await interaction.reply({
-            content: "Fill out this [form](https://youtu.be/fC7oUOUEEi4) first. Once you did, please DM the bot with your email address given for the form to verify your discord account.",
+            content: "Fill out this [form](<https://youtu.be/fC7oUOUEEi4>) first. Once you did, please DM the bot with your email address given for the form to verify your discord account.",
             ephemeral: true,
         });
     },
