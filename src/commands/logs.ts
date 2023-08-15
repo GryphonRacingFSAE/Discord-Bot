57315fc788d27f7dcbbe8e2a3ada79f33011038e
Danny Le <dannylewastaken@protonmail.com>
2023-08-14 21:50:51 -0400

    fix: logs not recognizing new lines + linting

--- src/commands/logs.ts
+++ src/commands/logs.ts
@@ -3,26 +3,28 @@
 import { CommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 import type { Command } from "@/types.js";
 import fs from "node:fs";
-import { EOL } from "os";
 
 const LOG_PATH = "./bot-logs.txt";
 const TEMP_LOG_PATH = "./bot-logs-temp.txt";
 
 export default {
-    data: new SlashCommandBuilder().setName("logs").setDescription("Get the last 4000 logs of the bot")
-        .addIntegerOption(option => option.setName("position").setDescription("End position of the logs to seek from. i.e. 4000 inputted would mean logs would range from 0->4000").setRequired(true) )
+    data: new SlashCommandBuilder()
+        .setName("logs")
+        .setDescription("Get the next 4000 logs of the bot")
+        .addIntegerOption(option => option.setName("position").setDescription("Beginning position to seek out logs").setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ViewAuditLog),
     async execute(interaction: CommandInteraction) {
-        let logs = fs.existsSync(LOG_PATH) ? fs.readFileSync(LOG_PATH, 'utf-8').split(EOL) : [];
-        const last_position = interaction.options.get("position")?.value! as number;
-        const start = Math.max(last_position - 4000, 0);
+        // Get position and next 4000 logs of the bot
+        const logs = fs.existsSync(LOG_PATH) ? fs.readFileSync(LOG_PATH, "utf-8").split(/\r?\n/) : [];
+        const position = Math.min(Math.max((interaction.options.get("position")?.value as number) || 0, 0), logs.length - 1);
+        const end_position = Math.min(position + 4000, logs.length - 1);
 
-        const last_logs = logs.slice(start, last_position).join(EOL);
+        const last_logs = logs.slice(position, end_position).join("\n");
         fs.writeFileSync(TEMP_LOG_PATH, last_logs); // Create temp file
 
         await interaction.reply({
             ephemeral: true,
-            files: [TEMP_LOG_PATH]
+            files: [TEMP_LOG_PATH],
         });
 
         fs.unlinkSync(TEMP_LOG_PATH); // Delete file
