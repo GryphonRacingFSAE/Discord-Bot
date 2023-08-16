a68d14f12f1328b8bea82e41e2b2790af9b7b8b4
Danny Le <dannylewastaken@protonmail.com>
2023-08-16 11:14:20 -0400

    feat: add code timeout

--- src/commands/verify.ts
+++ src/commands/verify.ts
@@ -5,10 +5,11 @@ export default {
     async execute(interaction: CommandInteraction) {
         const embeds = new EmbedBuilder()
             .setTitle("UofG Racing Verification")
-            .setDescription("Welcome! To **gain access to the server, please verify yourself.**")
+            .setDescription("Welcome! To gain access to the server, please verify yourself.")
             .addFields(
                 { name: "How", value: "1. Apply to this [form](<https://youtu.be/fC7oUOUEEi4>).\n2. **DM the bot** the email given to the form.\n3. Follow the instructions given." },
                 { name: "Accepted emails", value: "**Only @uoguelph.ca** are accepted emails." },
+                { name: "Code expiration", value: "Your code will **expire in 5 minutes**. If it has, please resend your email address and we will send you a new code." },
             )
             .setColor("#FFC72A")
             .setFooter({ text: "UofG racing will not ask for passwords, credit card information, SSNs, ID, and/or tokens" });
