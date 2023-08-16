19c9c085bb848f471d95d2c851c3789901fcd17f
Danny Le <dannylewastaken@protonmail.com>
2023-08-16 11:06:03 -0400

    feat: rename files using unix timestamp

--- src/commands/logs.ts
+++ src/commands/logs.ts
@@ -5,7 +5,6 @@ import type { Command } from "@/types.js";
 import fs from "node:fs";
 
 const LOG_PATH = "./bot-logs.txt";
-const TEMP_LOG_PATH = "./bot-logs-temp.txt";
 
 export default {
     data: new SlashCommandBuilder()
@@ -15,18 +14,23 @@ export default {
         .setDefaultMemberPermissions(PermissionFlagsBits.ViewAuditLog),
     async execute(interaction: CommandInteraction) {
         // Get position and next 4000 logs of the bot
-        const logs = fs.existsSync(LOG_PATH) ? fs.readFileSync(LOG_PATH, "utf-8").split(/\r?\n/) : [];
-        const position = Math.min(Math.max((interaction.options.get("position")?.value as number) || 0, 0), logs.length - 1);
-        const end_position = Math.min(position + 4000, logs.length - 1);
+        if (fs.existsSync(LOG_PATH)) {
+            const logs = fs.readFileSync(LOG_PATH, "utf-8").split(/\r?\n/);
+            const position = Math.min(Math.max((interaction.options.get("position")?.value as number) || 0, 0), logs.length - 1);
+            const end_position = Math.min(position + 4000, logs.length - 1);
+            const temp_log_path = Date.now().toString() + '.txt'; // Use timestamp
 
-        const last_logs = logs.slice(position, end_position).join("\n");
-        fs.writeFileSync(TEMP_LOG_PATH, last_logs); // Create temp file
+            const last_logs = logs.slice(position, end_position).join("\n");
+            fs.writeFileSync(temp_log_path, last_logs); // Create temp file
 
-        await interaction.reply({
-            ephemeral: true,
-            files: [TEMP_LOG_PATH],
-        });
+            await interaction.reply({
+                ephemeral: true,
+                files: [temp_log_path],
+            });
 
-        fs.unlinkSync(TEMP_LOG_PATH); // Delete file
+            fs.unlinkSync(temp_log_path); // Delete file
+        } else {
+            await interaction.reply({ephemeral: true, content: "Bot was not setup with logging."});
+        }
     },
 } as Command;
