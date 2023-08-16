fcc1b951fdac2e062fee4a2853514a0ea723847b
Danny Le <git@dannydoes.graphics>
2023-08-16 11:06:59 -0400

    lint

--- src/commands/logs.ts
+++ src/commands/logs.ts
@@ -18,7 +18,7 @@ export default {
             const logs = fs.readFileSync(LOG_PATH, "utf-8").split(/\r?\n/);
             const position = Math.min(Math.max((interaction.options.get("position")?.value as number) || 0, 0), logs.length - 1);
             const end_position = Math.min(position + 4000, logs.length - 1);
-            const temp_log_path = Date.now().toString() + '.txt'; // Use timestamp
+            const temp_log_path = Date.now().toString() + ".txt"; // Use timestamp
 
             const last_logs = logs.slice(position, end_position).join("\n");
             fs.writeFileSync(temp_log_path, last_logs); // Create temp file
@@ -30,7 +30,7 @@ export default {
 
             fs.unlinkSync(temp_log_path); // Delete file
         } else {
-            await interaction.reply({ephemeral: true, content: "Bot was not setup with logging."});
+            await interaction.reply({ ephemeral: true, content: "Bot was not setup with logging." });
         }
     },
 } as Command;
