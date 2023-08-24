f87dfb2e94f98f6710d8f4be60c4d19a81684b78
Danny Le <dannylewastaken@protonmail.com>
2023-08-24 13:35:58 -0400

    fix: add port as enviroment variable

--- src/vertification.ts
+++ src/vertification.ts
@@ -22,7 +22,7 @@ if (
 
 const transporter = createTransport({
     host: process.env.EMAIL_HOST,
-    port: 587,
+    port: +process.env.EMAIL_PORT,
     auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD,
