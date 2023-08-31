e69c939db814886b939458c8f344d1130e4cf662
Danny Le <git@dannydoes.graphics>
2023-08-31 19:37:28 -0400

    fix: switch around ordering of message replying + sending

--- src/commands/verify.ts
+++ src/commands/verify.ts
@@ -5,10 +5,10 @@ export default {
     data: new SlashCommandBuilder().setName("verify").setDescription("Verify yourself to become a member of Gryphon Racing!"),
     async execute(interaction: CommandInteraction) {
         if (interaction.user.bot) return;
-        await sendVerificationMessage(interaction.member as GuildMember);
         await interaction.reply({
             content: "Please check your DMs!",
             ephemeral: true,
         });
+        await sendVerificationMessage(interaction.member as GuildMember);
     },
 };
