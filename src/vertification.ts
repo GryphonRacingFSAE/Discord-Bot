98b35a5be4a55e7e23e3117513e9f75cab40b2de
Danny Le <git@dannydoes.graphics>
2023-10-20 12:03:31 -0400

    fix: bypass email already registered warnings if using legacy id

--- src/vertification.ts
+++ src/vertification.ts
@@ -341,7 +341,8 @@ export async function handleVerification(client: Client, message: Message) {
     // Make sure the email isn't already verified
     // If already verified make sure it's the same discord id
     const entry = verification_spreadsheet.find(entry => entry.email === message.content);
-    if (entry && entry.discord_identifier.length > 0 && entry.discord_identifier !== message.author.id) {
+    // If discord identifier contains a letter in it, it's using the legacy system and allow for re-verification
+    if (entry && entry.discord_identifier.length > 0 && (entry.discord_identifier !== message.author.id || /[a-zA-Z]/.test(entry.discord_identifier))) {
         await message.reply("Email is already registered with a different account's discord ID, please contact a `@Bot Developer` to resolve this issue.");
         return;
     }
