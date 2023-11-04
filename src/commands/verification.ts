38b51b70204880f0763098f0f9ec3f59ac3268ea
Danny Le <dannylewastaken@protonmail.com>
2023-11-04 01:39:23 -0400

    fix: Proper formatting + move queue in singleton + improve overriding logic

--- src/commands/verification.ts
+++ src/commands/verification.ts
@@ -7,54 +7,32 @@ import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 import type { Command } from "@/types.js";
 import { pullSpreadsheet, pushSpreadsheet, SpreadsheetSingleton } from "@/vertification.js";
 
-let verification_spreadsheet = SpreadsheetSingleton.getInstance();
+const verification_spreadsheet = SpreadsheetSingleton.getInstance();
 
 export default {
-    /*
-     * Define information about the bot required by discord.js
-     */
-    data: new SlashCommandBuilder().setName("verification")
+    data: new SlashCommandBuilder()
+        .setName("verification")
+        .setDescription("Manually modify the spreadsheet data of the verification roster")
         .addSubcommand(sub_command =>
             sub_command
                 .setName("edit")
-                .addStringOption(option =>
-                    option
-                        .setName("target")
-                        .setDescription("Full name to target. If no name exists, a new will be made and you will be notified of it.")
-                        .setRequired(true))
-                .addStringOption(option =>
-                    option
-                        .setName("email")
-                        .setDescription("Email address of user")
-                        .setRequired(false))
-                .addBooleanOption(option =>
-                    option
-                        .setName("gryphlife")
-                        .setDescription("Whether or not user is in GryphLife")
-                        .setRequired(false))
-                .addBooleanOption(option =>
-                    option
-                        .setName("paid")
-                        .setDescription("Whether or not user has paid")
-                        .setRequired(false))
-                .addStringOption(option =>
-                    option
-                        .setName("discord_id")
-                        .setDescription("Discord ID of user. THIS SHOULD NOT AT ALL BE USED UNLESS YOU KNOW WHAT YOU ARE DOING.")
-                        .setRequired(false))
+                .setDescription("Edit/add a row")
+                .addStringOption(option => option.setName("target").setDescription("Full name to target. If no name exists, a new will be made and you will be notified of it.").setRequired(true))
+                .addStringOption(option => option.setName("email").setDescription("Email address of user").setRequired(false))
+                .addBooleanOption(option => option.setName("gryphlife").setDescription("Whether or not user is in GryphLife").setRequired(false))
+                .addBooleanOption(option => option.setName("paid").setDescription("Whether or not user has paid").setRequired(false))
+                .addStringOption(option => option.setName("id").setDescription("Discord ID of user. THIS SHOULD NOT AT ALL BE USED UNLESS YOU KNOW WHAT YOU ARE DOING.").setRequired(false)),
         )
         .addSubcommand(sub_command =>
             sub_command
                 .setName("remove")
                 .setDescription("Remove a user from the spreadsheet which will wipe out their row")
-                .addStringOption(option =>
-                    option
-                        .setName("target")
-                        .setDescription("Full name to target.")
-                        .setRequired(true))),
+                .addStringOption(option => option.setName("target").setDescription("Full name to target.").setRequired(true)),
+        ),
 
     async execute(interaction: CommandInteraction) {
         if (!interaction.isChatInputCommand()) return;
+        /*
         {
             // Determine permission to use
             const guild = interaction.guild;
@@ -76,6 +54,7 @@ export default {
                 return;
             }
         }
+        */
         const options = interaction.options;
         switch (options.getSubcommand()) {
             case "edit": {
@@ -83,54 +62,70 @@ export default {
                 const EMAIL: string | undefined = options.get("email")?.value as string | undefined;
                 const GRYPHLIFE: boolean | undefined = options.get("gryphlife")?.value as boolean | undefined;
                 const PAID: boolean | undefined = options.get("paid")?.value as boolean | undefined;
-                const DISCORD_ID: string | undefined = options.get("discord_id")?.value as string | undefined; // Discord IDs should only be integers, BUT if we get a non-number, clear out the discord id
+                const DISCORD_ID: string | undefined = options.get("id")?.value as string | undefined; // Discord IDs should only be integers, BUT if we get a non-number, clear out the discord id
                 // Get the latest data first
-                await pullSpreadsheet().then(() => verification_spreadsheet.mutex.runExclusive( async () => {
-                    let USER_ROW_INDEX = verification_spreadsheet._data.findIndex( data => data.name.toLowerCase() === TARGET.toLowerCase());
-                    let data; // Verification data but is temporary for now
-                    if (USER_ROW_INDEX == -1) {
-                        // Does not exist! Create a new one!
-                        data = {
-                                name: TARGET,
-                                email: "",
-                                in_gryphlife: "",
-                                payment_status: "",
-                                discord_identifier: "",
-                        }
-                        verification_spreadsheet._data.push(data);
-                        USER_ROW_INDEX = verification_spreadsheet._data.length - 1;
-                        await interaction.reply({content: `Could not find: ${TARGET}, making a new one.`, ephemeral: true});
-                    } else {
-                        data = verification_spreadsheet._data[USER_ROW_INDEX];
-                    }
+                await pullSpreadsheet(verification_spreadsheet)
+                    .then(() =>
+                        verification_spreadsheet.mutex.runExclusive(async () => {
+                            let USER_ROW_INDEX = verification_spreadsheet._data.findIndex(data => data.name.toLowerCase() === TARGET.toLowerCase());
+                            let data; // Verification data but is temporary for now
+                            let new_data: boolean = false; // If this row has never been seen before
+                            if (USER_ROW_INDEX == -1) {
+                                // Does not exist! Create a new one!
+                                data = {
+                                    name: TARGET,
+                                    email: "",
+                                    in_gryphlife: "",
+                                    payment_status: "",
+                                    discord_identifier: "",
+                                };
+                                verification_spreadsheet._data.push(data);
+                                USER_ROW_INDEX = verification_spreadsheet._data.length - 1;
+                                new_data = true;
+                            } else {
+                                data = verification_spreadsheet._data[USER_ROW_INDEX];
+                            }
 
-                    // Override all data, if it has been filled in. If not, ignore it.
-                    data = {
-                        name: data.name,
-                        email: EMAIL ? EMAIL : data.email,
-                        in_gryphlife: GRYPHLIFE ? "yes" : data.in_gryphlife,
-                        payment_status: PAID ? "paid" : data.payment_status,
-                        discord_identifier: DISCORD_ID ? DISCORD_ID : data.discord_identifier
-                    };
-                    verification_spreadsheet._data[USER_ROW_INDEX] = data;
-                    await interaction.reply({content: `Edited \`${TARGET}\`'s data`, ephemeral: true});
-                })).then(() => pushSpreadsheet());
+                            // Override all data, if it has been filled in. If not, ignore it.
+                            data = {
+                                name: data.name,
+                                email: EMAIL !== undefined ? EMAIL : data.email,
+                                in_gryphlife: GRYPHLIFE ? "yes" : data.in_gryphlife,
+                                payment_status: PAID ? "paid" : data.payment_status,
+                                discord_identifier: DISCORD_ID !== undefined ? (/^\d+$/.test(DISCORD_ID) ? DISCORD_ID : "") : data.discord_identifier,
+                            };
+                            verification_spreadsheet._data_queue.push({
+                                index: USER_ROW_INDEX,
+                                row: data,
+                            });
+                            if (new_data) {
+                                await interaction.reply({ content: `Edited \`${TARGET}\`'s data. However this is never seen before data and as such a new row has been created.`, ephemeral: true });
+                            } else {
+                                await interaction.reply({ content: `Edited \`${TARGET}\`'s data`, ephemeral: true });
+                            }
+                        }),
+                    )
+                    .then(() => pushSpreadsheet(verification_spreadsheet));
 
                 break;
             }
             case "remove": {
                 const TARGET: string = options.get("target")?.value as string; // Only required
 
-                await pullSpreadsheet().then(() => verification_spreadsheet.mutex.runExclusive(  async () => {
-                    let USER_ROW_INDEX = verification_spreadsheet._data.findIndex( data => data.name.toLowerCase() === TARGET.toLowerCase());
-                    if (USER_ROW_INDEX == -1) {
-                        // Does not exist! Cannot remove at all.
-                        await interaction.reply({content: `Could not find: ${TARGET}.`, ephemeral: true});
-                    } else {
-                        verification_spreadsheet._data.splice(USER_ROW_INDEX, 1);
-                        await interaction.reply({content: `Removed \`${TARGET}\` 's data`, ephemeral: true});
-                    }
-                })).then(() => pushSpreadsheet());
+                await pullSpreadsheet(verification_spreadsheet)
+                    .then(() =>
+                        verification_spreadsheet.mutex.runExclusive(async () => {
+                            const USER_ROW_INDEX = verification_spreadsheet._data.findIndex(data => data.name.toLowerCase() === TARGET.toLowerCase());
+                            if (USER_ROW_INDEX == -1) {
+                                // Does not exist! Cannot remove at all.
+                                await interaction.reply({ content: `Could not find \`${TARGET}\``, ephemeral: true });
+                            } else {
+                                verification_spreadsheet._data.splice(USER_ROW_INDEX, 1);
+                                await interaction.reply({ content: `Removed \`${TARGET}\` 's data`, ephemeral: true });
+                            }
+                        }),
+                    )
+                    .then(() => pushSpreadsheet(verification_spreadsheet));
             }
         }
     },
