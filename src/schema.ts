46eb4cd4f01cee0a11758f837765d0dd75fea88c
Danny Le <dannylewastaken@protonmail.com>
2024-03-11 15:17:18 -0400

    feat: add first name and last name

--- src/schema.ts
+++ src/schema.ts
@@ -1,12 +1,19 @@
 /* tslint:disable:no-unused-variable */
-import { mysqlTable, text, boolean, int, datetime, varchar } from "drizzle-orm/mysql-core";
+import { mysqlTable, text, boolean, int, datetime, varchar, unique } from "drizzle-orm/mysql-core";
 import { sql } from "drizzle-orm";
 
+/**
+ * @description Schema for how we store the credentials of everyone
+ *
+ * Email and discord ids should only be unique.
+ */
 export const users = mysqlTable("users", {
     email: varchar("email", { length: 255 }).primaryKey().notNull(),
     discordId: varchar("discord_id", { length: 32 }).notNull().default("").unique(),
     paymentStatus: boolean("payment_status").notNull().default(false),
     gryphLife: boolean("in_gryphlife").notNull().default(false),
+    firstName: varchar("first_name", { length: 64 }),
+    lastName: varchar("last_name", { length: 64 }),
 });
 
 export const verifying_users = mysqlTable("verifying_users", {
